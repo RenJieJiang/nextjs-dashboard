5b757ad5f0ec0bec2ec1cf842913d1f76de93c7f
RenJieJiang <jiangrj1@hotmail.com>
2025-05-03 00:50:21 +1000

    feat: add login

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -11,11 +11,10 @@ import {
 import { Button } from '@/app/ui/button';
 import { createInvoice, State } from '@/app/lib/actions';
 import { useActionState } from 'react';
-import { useFormState } from 'react-dom';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = { message: null, errors: {} };
-  const [state, formAction] = useFormState(createInvoice, initialState);
+  const [state, formAction] = useActionState(createInvoice, initialState);
 
   return (
     <form action={formAction}>
